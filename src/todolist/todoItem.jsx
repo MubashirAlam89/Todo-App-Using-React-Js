f261514f046135bb009309cb7307e9ee32feddfc
mubashiralam89 <mubashiralam89@gmail.com>
2023-06-08 01:10:30 +0500

    added a taskCompleted icon and created an taskCompleted function

--- src/todolist/todoItem.jsx
+++ src/todolist/todoItem.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
 import DeleteIcon from "../assets/deleteicon.png";
+import TickIcon from "../assets/tickicon.png";
 import "./todo.css";
 
 export const TodoItem = ({ title, deleteTodo }) => {
+  const taskCompleted = (e) => {
+    e.target.parentElement.previousSibling.style.textDecoration =
+      "line-through";
+  };
   return (
     <div className="todo-item" id="todo-1">
       <p className="todo-task-title">{title}</p>
-      <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
+      <div className="icons-container">
+        <img className="todo-icon" src={TickIcon} onClick={taskCompleted} />
+        <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
+      </div>
     </div>
   );
 };
